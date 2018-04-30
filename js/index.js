e8319dc328e4043e634c3c3426373ec9c8be1dfe
Yuliya Stefanyshyn <julia.sokil.1993@gmail.com>
2018-04-30 16:01:47 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,77 @@
+//alert ('Hello');
+//document.write('<h1>World</h1>');
+//console.log('Hello World');
+
+// let carName ='Tesla Model X';
+// const purchaseCarPrice = 100000 + 10000 ;
+// let isCarAvailable = true;
+// document.write(carName);
+// document.write('<br>');
+// document.write(purchaseCarPrice);
+// document.write('<br>');
+// document.write(isCarAvailable);
+
+// const saleCarPrice=purchaseCarPrice*1.1;
+
+// const age = parseInt(prompt('Enter your age'));
+// const newAge = age +1;
+// document.write('Next year you will be'+ newAge);
+
+// // Number
+// // 1, NaN, Infinity
+
+// //String
+// // "", '', ``
+
+// //Boolean
+// //true, false
+
+// //undefined
+// var variable;
+// let anotherVariable;
+
+// //null
+// variable=null;
+
+// document.write('Hello' + Number(variable) = Number(anothervariable))
+
+
+// //object
+// const car = {};
+
+// //Symbol
+// const sym = Symbol();
+
+// const age = parseInt(prompt('Enter your age'));
+
+// if ((age >= 18) && (age <70)) {
+// document.write('You are adult');
+// }
+// else if (age < 18) {
+// document.write('You are a child');    
+// }
+// else {
+//     document.write('You are old');
+// }
+
+// function add(a, b, c) {
+//     const result = a + b + c;
+//     return result;
+// }
+
+// const sum = add(100, 200, 300);
+// document.write(sum);
+
+document.write(Math.sqrt(16));
+
+
+
+
+
+
+
+
+
+
+
+
